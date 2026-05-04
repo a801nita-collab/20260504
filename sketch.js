1e34990571e6dc0db9d7c7733d646d751ed480e8
a801nita-collab <a801nita@gmail.com>
2026-05-04 10:31:33 +0800

    Add faceMesh detection and drawing functionality

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
 let capture;
+let faceMesh;
+let faces = [];
+let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
+
+function preload() {
+  // 載入 ml5.js 的 faceMesh 模型
+  faceMesh = ml5.faceMesh(options);
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // 啟動視訊擷取
   capture = createCapture(VIDEO);
+  capture.size(640, 480);
   // 隱藏預設產生的 HTML video 元件，我們只在 canvas 裡畫它
   capture.hide();
+
+  // 開始偵測臉部
+  faceMesh.detectStart(capture, gotFaces);
 }
 
 function draw() {
@@ -24,7 +36,36 @@ function draw() {
   translate(x + displayWidth, y);
   scale(-1, 1);
   image(capture, 0, 0, displayWidth, displayHeight);
+
+  // 繪製臉部線條
+  if (faces.length > 0) {
+    let face = faces[0];
+    let points = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+    
+    noFill();
+    stroke('red');
+    strokeWeight(15);
+    strokeJoin(ROUND);
+    
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+      let index = points[i];
+      let keypoint = face.keypoints[index];
+      
+      // 將座標從原始影片大小映射到顯示大小 (50% 畫面)
+      let sx = keypoint.x * (displayWidth / capture.width);
+      let sy = keypoint.y * (displayHeight / capture.height);
+      
+      vertex(sx, sy);
+    }
+    endShape();
+  }
   pop();
+}
+
+// 取得偵測結果的回呼函式
+function gotFaces(results) {
+  faces = results;
 }
 
 function windowResized() {
